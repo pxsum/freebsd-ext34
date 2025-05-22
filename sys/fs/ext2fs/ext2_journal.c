7b3f0f2ee79d91313f9f664bf563b1a8b633b860
pxsum <pau@sumpau.com>
2025-05-22 11:33:37 -0400

    Add journal superblock endianness converstion
    Journal superblocks are big-endian on disk but should use host byte
    order in memory. Add conversion functions to handle this translation
    between disk and in-memory representations.

--- sys/fs/ext2fs/ext2_journal.c
+++ sys/fs/ext2fs/ext2_journal.c
@@ -63,13 +63,59 @@ ext2_verify_journal_block(void *data)
 {
 	struct ext2fs_journal_block_header *jrn_bhr =
 		(struct ext2fs_journal_block_header *) data;
-	return (EXT2_JBSWAP32(jrn_bhr->jbh_magic) == EXT2_JOURNAL_MAGIC);
+	return (be32toh(jrn_bhr->jbh_magic) == EXT2_JOURNAL_MAGIC);
 }
 
-// TODO
 static void
-ext2_jsb_bswap(struct ext2fs_journal_sb *old, struct ext2fs_journal_sb *new)
+ext2_jsb_from_disk(struct ext2fs_journal_sb *host_jsb,
+    struct ext2fs_journal_sb *disk_jsb)
 {
+	host_jsb->jsb_header.jbh_magic = be32toh(disk_jsb->jsb_header.jbh_magic);
+	host_jsb->jsb_header.jbh_blocktype = be32toh(disk_jsb->jsb_header.jbh_blocktype);
+	host_jsb->jsb_header.jbh_sequence_num = be32toh(disk_jsb->jsb_header.jbh_sequence_num);
+	host_jsb->jsb_blocksize = be32toh(disk_jsb->jsb_blocksize);
+	host_jsb->jsb_max_blocks = be32toh(disk_jsb->jsb_max_blocks);
+	host_jsb->jsb_first_block = be32toh(disk_jsb->jsb_first_block);
+	host_jsb->jsb_sequence_id = be32toh(disk_jsb->jsb_sequence_id);
+	host_jsb->jsb_start_block_num = be32toh(disk_jsb->jsb_start_block_num);
+	host_jsb->jsb_errno = be32toh(disk_jsb->jsb_errno);
+	host_jsb->jsb_feature_compat = be32toh(disk_jsb->jsb_feature_compat);
+	host_jsb->jsb_feature_incompat = be32toh(disk_jsb->jsb_feature_incompat);
+	host_jsb->jsb_feature_ro_compat = be32toh(disk_jsb->jsb_feature_ro_compat);
+	host_jsb->jsb_num_users = be32toh(disk_jsb->jsb_num_users);
+	host_jsb->jsb_dynamic_sb = be32toh(disk_jsb->jsb_dynamic_sb);
+	host_jsb->jsb_trans_max = be32toh(disk_jsb->jsb_trans_max);
+	host_jsb->jsb_trans_data_max = be32toh(disk_jsb->jsb_trans_data_max);
+	host_jsb->jsb_checksum_type = be32toh(disk_jsb->jsb_checksum_type);
+
+	memcpy(host_jsb->jsb_uuid, disk_jsb->jsb_uuid, 16);
+	memcpy(host_jsb->jsb_padding2, disk_jsb->jsb_padding2, 3);
+}
+
+static void
+ext2_jsb_to_disk(struct ext2fs_journal_sb *disk_jsb,
+    struct ext2fs_journal_sb *host_jsb)
+{
+	disk_jsb->jsb_header.jbh_magic = htobe32(host_jsb->jsb_header.jbh_magic);
+	disk_jsb->jsb_header.jbh_blocktype = htobe32(host_jsb->jsb_header.jbh_blocktype);
+	disk_jsb->jsb_header.jbh_sequence_num = htobe32(host_jsb->jsb_header.jbh_sequence_num);
+	disk_jsb->jsb_blocksize = htobe32(host_jsb->jsb_blocksize);
+	disk_jsb->jsb_max_blocks = htobe32(host_jsb->jsb_max_blocks);
+	disk_jsb->jsb_first_block = htobe32(host_jsb->jsb_first_block);
+	disk_jsb->jsb_sequence_id = htobe32(host_jsb->jsb_sequence_id);
+	disk_jsb->jsb_start_block_num = htobe32(host_jsb->jsb_start_block_num);
+	disk_jsb->jsb_errno = htobe32(host_jsb->jsb_errno);
+	disk_jsb->jsb_feature_compat = htobe32(host_jsb->jsb_feature_compat);
+	disk_jsb->jsb_feature_incompat = htobe32(host_jsb->jsb_feature_incompat);
+	disk_jsb->jsb_feature_ro_compat = htobe32(host_jsb->jsb_feature_ro_compat);
+	disk_jsb->jsb_num_users = htobe32(host_jsb->jsb_num_users);
+	disk_jsb->jsb_dynamic_sb = htobe32(host_jsb->jsb_dynamic_sb);
+	disk_jsb->jsb_trans_max = htobe32(host_jsb->jsb_trans_max);
+	disk_jsb->jsb_trans_data_max = htobe32(host_jsb->jsb_trans_data_max);
+	disk_jsb->jsb_checksum_type = htobe32(host_jsb->jsb_checksum_type);
+
+	memcpy(disk_jsb->jsb_uuid, host_jsb->jsb_uuid, 16);
+	memcpy(disk_jsb->jsb_padding2, host_jsb->jsb_padding2, 3);
 }
 
 static int
@@ -103,9 +149,9 @@ ext2_journal_open_inode(struct mount *mp, struct vnode **vpp,
 	}
 
 	jrn_sbp = (struct ext2fs_journal_sb *) jrn_data;
-	if (EXT2_JBSWAP32(jrn_sbp->jsb_header.jbh_blocktype) !=
+	if (be32toh(jrn_sbp->jsb_header.jbh_blocktype) !=
 	    EXT2_JOURNAL_FORMAT_BASIC &&
-	    EXT2_JBSWAP32(jrn_sbp->jsb_header.jbh_blocktype) !=
+	    be32toh(jrn_sbp->jsb_header.jbh_blocktype) !=
 	    EXT2_JOURNAL_FORMAT_EXTENDED)
 	    {
 		    brelse(jrn_buf);
@@ -117,7 +163,7 @@ ext2_journal_open_inode(struct mount *mp, struct vnode **vpp,
 	*jrn_sbpp = (struct ext2fs_journal_sb *)
 		malloc(sizeof(struct ext2fs_journal_sb), M_EXT2JSB, M_WAITOK);
 
-	ext2_jsb_bswap(jrn_sbp, *jrn_sbpp);
+	ext2_jsb_from_disk(*jrn_sbpp, jrn_sbp);
 	brelse(jrn_buf);
 	VOP_UNLOCK(*vpp);
 	return (0);
